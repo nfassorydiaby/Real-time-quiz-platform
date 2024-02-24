e72c803e8b50c3a804ce091ded19f73390598574
Ricardo <abrahamricardogn224@gmail.com>
2024-02-24 21:23:53 +0100

    add of chat

--- back/main.py
+++ back/main.py
@@ -8,7 +8,8 @@ from fastapi import Body
 from fastapi.middleware.cors import CORSMiddleware
 import auth
 from pydantic import BaseModel
-
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 
 # import auth
 
@@ -21,7 +22,9 @@ from sqlalchemy.orm import Session
 app = FastAPI(
     title="Real Time Quiz Plateform",
     description="This API aim to provide feature to manage a graphical interface for Real Time Quiz Plateform.")
+
 app.include_router(auth.router)
+
 origins = [
     "http://localhost:3000",
     "http://localhost:3001",
@@ -36,6 +39,50 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Websocket Demo</title>
+           <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    </head>
+    <body>
+    <div class="container mt-3">
+        <h1>FastAPI WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" class="form-control" id="messageText" autocomplete="off"/>
+            <button class="btn btn-outline-primary mt-2">Send</button>
+        </form>
+        <ul id='messages' class="mt-5">
+        </ul>
+        
+    </div>
+    
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
 # app.include_router(auth.router)
 # create the database
@@ -57,24 +104,29 @@ def getDb():
 
 dbDependency = Annotated[Session, Depends(getDb)]
 
+
 class QuizResponse(BaseModel):
     id: int
     title: str
     description: str
     creator_id: int
 
+
 class CreateQuizRequest(BaseModel):
     title: str
     description: str
     creator_id: int
+
 
 class QuestionResponse(BaseModel):
     id: int
     question_text: str
     quiz_id: int
 
+
 class CreateQuestionRequest(BaseModel):
     question_text: str
+
 
 class OptionResponse(BaseModel):
     id: int
@@ -82,9 +134,11 @@ class OptionResponse(BaseModel):
     question_id: int
     is_correct: bool
 
+
 class CreateOptionRequest(BaseModel):
     option_text: str
     is_correct: bool
+
 
 @app.get("/quizs/", status_code=status.HTTP_200_OK, tags=["Quizs"])
 async def readQuiz(db: dbDependency):
@@ -103,30 +157,75 @@ async def createQuiz(quiz: CreateQuizRequest, db: dbDependency):
 
 @app.get("/quizs/{quizId}/question/", status_code=status.HTTP_200_OK, tags=["Questions"])
 async def readQuestions(db: dbDependency, quizId: int):
-    questions = db.query(models.Question).filter(models.Question.quiz_id == quizId).all()
+    questions = db.query(models.Question).filter(
+        models.Question.quiz_id == quizId).all()
     return questions
+
 
 @app.post('/quizs/{quizId}/question/', response_model=QuestionResponse, status_code=status.HTTP_201_CREATED, tags=["Questions"])
 async def createQuestion(question: CreateQuestionRequest, db: dbDependency, quizId: int):
     question_data = question.dict()
-    question_data['quiz_id'] = quizId 
+    question_data['quiz_id'] = quizId
     dbQuestion = models.Question(**question_data)
     db.add(dbQuestion)
     db.commit()
     db.refresh(dbQuestion)
     return dbQuestion
 
+
 @app.get("/question/{questionId}/options", status_code=status.HTTP_200_OK, tags=["Options"])
 async def readOptions(db: dbDependency, questionId: int):
-    questions = db.query(models.Option).filter(models.Option.question_id == questionId).all()
+    questions = db.query(models.Option).filter(
+        models.Option.question_id == questionId).all()
     return questions
+
 
 @app.post('/question/{questionId}/options', response_model=OptionResponse, status_code=status.HTTP_201_CREATED, tags=["Options"])
 async def createOptions(option: CreateOptionRequest, db: dbDependency, questionId: int):
     option_data = option.dict()
-    option_data['question_id'] = questionId 
+    option_data['question_id'] = questionId
     dbOption = models.Option(**option_data)
     db.add(dbOption)
     db.commit()
     db.refresh(dbOption)
     return dbOption
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} has left the chat")
